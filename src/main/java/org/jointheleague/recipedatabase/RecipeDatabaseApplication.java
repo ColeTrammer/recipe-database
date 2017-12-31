f693c999d99944fd53e766f0cc12ac60cf2cf48a
League <league@WTS10.attlocal.net>
2017-12-31 13:40:34 -0800

    initial commit

---
+++ src/main/java/org/jointheleague/recipedatabase/RecipeDatabaseApplication.java
@@ -0,0 +1,12 @@
+package org.jointheleague.recipedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeDatabaseApplication.class, args);
+	}
+}
